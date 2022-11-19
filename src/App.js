258d1fa2e8461e90316ee4c689a4995d259840c3
Folashy <100294216+Folashy@users.noreply.github.com>
2022-11-19 20:52:34 +0100

    contact us

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Page1 from "./pages/page1/page1";
+import Page2 from "./pages/page2/page2";
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Page1 />} />
+          <Route path="/places" element={<Page2 />} />
+        </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/page1/page1.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Formheader from "../../comonents/formHeader/form";
+import Header from "../../comonents/header/header";
+
+export default function Page1() {
+  return (
+    <>
+      <Header />
+      <Formheader />
+     
+    </>
+  );
+}
